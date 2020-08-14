d00188e5e03b3c362e79bb23b4b29894a96f2407
motya1121 <s18019@g.metro-cit.ac.jp>
2020-08-15 00:49:40 +0900

    [update] 特殊データに対応

--- tool/covid_data/get_covid_data.py
+++ tool/covid_data/get_covid_data.py
@@ -33,6 +33,8 @@ class patient_data():
                 if self.no != '':  # noの場合関数終了
                     return
 
+            if self.no == '315':
+                self.status_id = 5
             if self.no == '322':
                 self.symptom = '発熱、咳、呼吸困難、倦怠感、筋肉痛、味覚・嗅覚障害、下痢'
             if self.no == '323':
