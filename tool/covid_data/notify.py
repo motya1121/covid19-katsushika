94f7f389484969a3059694d39f8a486fa3c862c8
motya1121 <s18019@g.metro-cit.ac.jp>
2020-08-28 18:58:13 +0900

    [fix] notifyを修正

--- tool/covid_data/notify.py
+++ tool/covid_data/notify.py
@@ -7,13 +7,16 @@ import urllib
 
 def notify(setting):
     setting_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "secret/slack.json")
-    ichiran = setting.pdf_url
-    syukei = setting.pdf_url.replace('itiran', 'jokyo')
+    ichiran = setting.pdf_urls
+    ichiran_msg = ""
+    for i in range(0,len(setting.pdf_urls)):
+        ichiran_msg += '<{0}|感染者一覧{1}>\n'.format(setting.pdf_urls[i], i)
+    syukei = setting.pdf_urls[0].replace('itiran1', 'joukyou')
     with open(setting_file) as f:
         setting_json = json.load(f)
         headers = {'Content-Type': 'application/json'}
-        message = '感染者情報が更新されました．\n<{0}|葛飾区内の新型コロナウイルス感染者発生状況>\n<{1}|感染者一覧>\n<{2}|感染者属性別集計表>'.format(
-            setting.city_url, ichiran, syukei)
+        message = '感染者情報が更新されました．\n<{0}|葛飾区内の新型コロナウイルス感染者発生状況>\n{1}<{2}|感染者属性別集計表>'.format(
+            setting.city_url, ichiran_msg, syukei)
 
         post_data = {'blocks': [{'type': 'section', 'text': {'type': 'mrkdwn', 'text': message}}]}
         req = urllib.request.Request(setting_json['slack_url'],
